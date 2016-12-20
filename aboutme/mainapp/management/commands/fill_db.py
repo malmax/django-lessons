12933b9fc34fd1547aafd951544f73c371dbe47e
malmax <malmax.spb@gmail.com>
2016-12-20 14:57:18 +0300

    complete task1

---
+++ aboutme/mainapp/management/commands/fill_db.py
@@ -0,0 +1,17 @@
+from django.core.management.base import BaseCommand
+from mainapp.models import Works, Hobby, Learns, Organization
+import os
+
+
+class Command(BaseCommand):
+    help = 'Fill DB new data'
+
+    def handle(self, *args, **options):
+
+        Organization.objects.all().delete()
+        Works.objects.all().delete()
+        Hobby.objects.all().delete()
+        Learns.objects.all().delete()
+
+        os.system("python manage.py loaddata data.json")
+
