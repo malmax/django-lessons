2cd57dfb464290ebe7955fa05d65c223225d0a03
malmax <malmax.spb@gmail.com>
2016-12-20 22:51:08 +0300

    init gameshop

---
+++ gameshop/shopbase/models.py
@@ -0,0 +1,61 @@
+from django.db import models
+
+# Игра-продукт. Их будет много в одном дисплее
+class GameProduct(models.Model):
+    old_nid = models.IntegerField(verbose_name="NID из старой версии сайта", default=0, unique=True)
+    title = models.CharField(max_length=150, verbose_name="Наименование игры, Battlefield [PS4]")
+    price = models.DecimalField(max_digits=5, decimal_places=0, verbose_name="Цена продажи")
+    cost = models.DecimalField(max_digits=5, decimal_places=0, verbose_name="Закупка")
+    stock = models.SmallIntegerField(verbose_name="Склад", default=0)
+    canOrder = models.BooleanField(verbose_name="Под заказ: True - можно заказать даже если склад = 0", default=False)
+    platformCategory = models.ForeignKey('PlatformCategory', verbose_name="Термин Платформа")
+    releaseDate = models.CharField(max_length=20, verbose_name="Дата выхода", null=True, blank=True)
+    commingDate = models.CharField(max_length=20, verbose_name="Дата поступления", null=True, blank=True)
+    cover = models.ImageField(upload_to="gameproduct_covers", verbose_name="Ссылка на обложку")
+    used = models.BooleanField(verbose_name="Игра Б/У?", default=False)
+    basenid = models.IntegerField(verbose_name="Nid БАЗЫ из gamebuy", default=0)
+    language = models.ForeignKey("LanguageCategory", verbose_name="ссылка на таблицу с языком", default=1)
+    GAMETYPE_ARR = [(1, "Коллекционное издание"),
+                    (2, "Сборник"),
+                    (3, "Издание Игра года"),
+                    (4, "Переиздание Platinum"),
+                    (5, "Переиздание Classic"),
+                    (6, "Переиздание Essential"),
+                    (7, "Переиздание Nintendo Selects"),
+                    (8, "Jewel"),
+                    (9, "Специальное издание"),
+                    (10, "Дополнение"),
+                    (11, "Collection"),
+                    (12, "Remaster"),
+                    (13, "Day One Edition"),
+                    (100, "Трилогия"),  # TODO: удалить на рабочем сайте
+                    (101, "DS")]  # TODO: удалить на рабочем сайте
+    gameType = models.SmallIntegerField(choices=GAMETYPE_ARR, blank=True, null=True, default=None)
+
+    def gametypeSet(self, text):
+        if text is not None:
+            for temp in self.GAMETYPE_ARR:
+                if (temp[1] == text):
+                    self.gameType = temp[0]
+                    break
+
+    def gametypeGet(self):
+        for temp in self.GAMETYPE_ARR:
+            if (temp[0] == self.gameType):
+                return " " + temp[1]
+        return ""
+
+    def canOrderSet(self, stock):
+        if (stock <= -10):
+            self.canOrder = True
+            self.stock += 10
+        else:
+            self.canOrder = False
+
+    # обновляем значение склада
+    def updateStock(self):
+        self.stock = self.stock
+        self.canOrderSet(self, self.stock)
+
+    def __str__(self):
+        return self.title
---
+++ gameshop/users/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, Http404, HttpResponseRedirect
+from django.contrib.auth import authenticate, login, logout
+
+# Create your views here.
+def login(request):
+    if request.method == 'POST':
+        username = request.POST.get('login')
+        password = request.POST.get('password')
+
+        user = authenticate(username = username, password = password)
+        if user:
+            login(request, user)
+            return HttpResponseRedirect('/')
+        else:
+            return render(request, 'index.html', {'username': username, 'errors': True})
+
+    raise Http404
+
+def logout(request):
+    logout(request)
+    HttpResponseRedirect('/')
